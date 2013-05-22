a97775581495df9d54ce439909dc093b11abf374
Jean Matheus <jeanmatheussouto@gmail.com>
2013-05-22 11:51:45 -0300

    adicionado keys do aplicativo do facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['347227862067152'], ENV['af279161146060d06d449ef5d85a0c08']
 end
